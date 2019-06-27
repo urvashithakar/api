2923f49b387fb22a90d74cccab304c4b930cac35
Urvashi Thakar <urvashi@theharsh.in>
2019-06-27 16:28:07 +0530

    Multiple file type issue

--- src/core/Directus/Database/TableGateway/RelationalTableGateway.php
+++ src/core/Directus/Database/TableGateway/RelationalTableGateway.php
@@ -609,7 +609,7 @@ class RelationalTableGateway extends BaseTableGateway
             }
 
             // Update/Add foreign record
-            if ($this->recordDataContainsNonPrimaryKeyData($foreignRow, $foreignTableSchema->getPrimaryKeyName()) && $field->getType() != "file") {
+            if ($this->recordDataContainsNonPrimaryKeyData($foreignRow, $foreignTableSchema->getPrimaryKeyName()) && ($field->getType() != "file" && !isset($parentRow[$fieldName][$primaryKey]))) {
                 // NOTE: using manageRecordUpdate instead of addOrUpdateRecordByArray to update related data
                 $foreignRow = $ForeignTable->manageRecordUpdate($foreignTableName, $foreignRow);
             }
