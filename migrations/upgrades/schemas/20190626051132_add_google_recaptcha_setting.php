852c26c447ff83e2239c42d4a0284b5ef909800c
Urvashi Thakar <urvashi@theharsh.in>
2019-06-27 12:41:14 +0530

    Added google client key in settings table

--- migrations/upgrades/schemas/20190626051132_add_google_recaptcha_setting.php
+++ migrations/upgrades/schemas/20190626051132_add_google_recaptcha_setting.php
@@ -8,21 +8,34 @@ class AddGoogleRecaptchaSetting extends AbstractMigration
     public function up()
     {
         $fieldObject = [
-            'field' => 'google_recaptcha_secret',
-            'type' => 'string',
-            'interface' => 'text-input',
-            'sort' => '14'
+            'google_recaptcha_secret' => [
+                'type' => 'string',
+                'interface' => 'text-input',
+                'sort' => '14',
+                'width' => 'half'
+            ],
+            'google_recaptcha_site_key' => [
+                'type' => 'string',
+                'interface' => 'text-input',
+                'sort' => '15',
+                'width' => 'half'
+            ],
         ];
-        $collection = 'directus_settings';
 
-        $checkSql = sprintf('SELECT 1 FROM `directus_fields` WHERE `collection` = "%s" AND `field` = "%s";', $collection, $fieldObject['field']);
+        foreach ($fieldObject as $field => $options) {
+            $this->addField($field, $options['type'], $options['interface'],$options['sort'],$options['width']);
+        }
+    }
+    protected function addField($field, $type, $interface, $sort ,$width)
+    {
+        $collection = 'directus_settings';
+        $checkSql = sprintf('SELECT 1 FROM `directus_fields` WHERE `collection` = "%s" AND `field` = "%s";', $collection, $field);
         $result = $this->query($checkSql)->fetch();
 
         if (!$result) {
-            $insertSqlFormat = 'INSERT INTO `directus_fields` (`collection`, `field`, `type`, `interface`, `sort`) VALUES ("%s", "%s", "%s", "%s", "%s");';
-            $insertSql = sprintf($insertSqlFormat, $collection, $fieldObject['field'], $fieldObject['type'], $fieldObject['interface'],$fieldObject['sort']);
+            $insertSqlFormat = 'INSERT INTO `directus_fields` (`collection`, `field`, `type`, `interface`, `sort`, `width`) VALUES ("%s", "%s", "%s", "%s", "%s", "%s");';
+            $insertSql = sprintf($insertSqlFormat, $collection, $field, $type, $interface, $sort ,$width);
             $this->execute($insertSql);
         }
-
     }
 }
